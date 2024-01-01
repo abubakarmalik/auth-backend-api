5f9c723459b14099069c3459fffd36593585e041
abubakarmalik <abubakarzoomii@gmail.com>
2024-01-01 23:38:48 +0500

    feat main route

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/', (req, res) => {
+  res.json({ message: 'Auth APi' });
+});
 app.use('/api/', userRoutes);
 app.use('/api/', dashboardRoutes);
 
